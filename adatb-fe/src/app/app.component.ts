720ef7d82e7790b0dc6532de6877f3bfbece9ba5
kunklaa <79965712+kunklaa@users.noreply.github.com>
2022-03-30 23:28:49 +0200

    BE bekötés, lektor táblából lekérdezés

--- adatb-fe/src/app/app.component.ts
+++ adatb-fe/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppComponent implements OnInit{
   title = 'adatb-fe';
+  lektorok = []; 
+
+constructor(private http: HttpClient){}
+
+  getLektorok() {
+    return this.http.get<any>(environment.API_URL + "/api/Lektor");
+  }
+
+  showLektorok() {
+    this.getLektorok()
+      .subscribe((data: any) => {
+        this.lektorok = data
+        console.log(this.lektorok)
+      });
+      
+  }
+
+  ngOnInit(): void {
+    this.showLektorok();
+    
+  }
 }
