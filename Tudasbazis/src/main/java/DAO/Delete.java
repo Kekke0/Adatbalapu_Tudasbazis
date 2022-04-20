5411dd5f88067030017c70b8f3202d2648f865a8
KaszaLeila <97163215+KaszaLeila@users.noreply.github.com>
2022-04-20 21:00:13 +0200

    Update Delete.java
    Bocsi ha nagyon nem így kéne :/

--- Tudasbazis/src/main/java/DAO/Delete.java
+++ Tudasbazis/src/main/java/DAO/Delete.java
@@ -26,4 +26,79 @@ public class Delete extends ConnectionProtocol {
         Stop();
         return rs>0;
     }
+    
+      public Boolean LektorID(String ID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete lektor where ID = ?");
+        stmt.setString(1,ID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    public Boolean NyelvtudasLektorID(String LektorID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete nyelvtudas where LektorID = ?");
+        stmt.setString(1,LektorID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    public Boolean AdminID(String ID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete admin where ID = ?");
+        stmt.setString(1,ID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    public Boolean KategoriaNev(String Nev) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete kategoria where Nev = ?");
+        stmt.setString(1,ID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    public Boolean CikkID(String ID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete cikk where ID = ?");
+        stmt.setString(1,ID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    public Boolean KulcsszoCimID(String CimID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete kulcsszo where CimID = ?");
+        stmt.setString(1,CimID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    public Boolean BanFelhaszID(String FelhasznaloID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete ban where FelhasznaloID = ?");
+        stmt.setString(1,FelhasznaloID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+      public Boolean ModositasCikkId(String ID) throws Exception{
+        Start();
+        PreparedStatement stmt=super.getConn().prepareStatement("delete felhasznalo where ID = ?");
+        stmt.setString(1,ID);
+        rs= stmt.executeUpdate();
+        Stop();
+        return rs>0;
+    }
+    
+    
+    
 }
